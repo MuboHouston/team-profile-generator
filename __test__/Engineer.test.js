57bcc74a2e872c6456c263baa73c75eccc1b90f4
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-02-25 11:23:02 -0600

    passing engineer test

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,7 +1,7 @@
 const Engineer = require('../lib/Engineer');
 
 test('creates an engineer object', () => {
-    const engineer = new Engineer("github");
+    const engineer = new Engineer("Ashley", 1, "testing@abc.com", "githubUsername");
 
     expect(engineer.github).toEqual(expect.any(String));
 })
