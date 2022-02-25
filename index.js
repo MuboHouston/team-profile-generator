95e83b580b70458d8e16474527bd8bddc486e060
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-02-25 14:55:45 -0600

    add manager questions

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 const generatePage = require('./src/generateHTML');
+
+const inquirer = require('inquirer')
+
+const managerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter the manger's name!",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the manager's name!")
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter the manager's ID number!",
+            validate: idInput => {
+                if(isNaN(idInput)) {
+                    console.log("Please enter the manager's ID number!")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter the manger's email address!",
+            validate: emailInput => {
+                if(emailInput) {
+                    return true
+                } else {
+                    console.log("Please enter the manager's email address!")
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Please enter manger's office number!",
+            validate: numberInput => {
+                if(isNaN(numberInput)) {
+                    console.log("Please enter manger's phone number")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "list",
+            name: "next",
+            message: "Would you like to add an engineer or an intern or to finish building team (Choose One)?",
+            choices: ["Add Engineer", "Add Intern", "Done"]
+        }
+    ])
+    .then(managerInput => {
+        console.log(managerInput);
+    })
+}
+
+managerQuestions();
