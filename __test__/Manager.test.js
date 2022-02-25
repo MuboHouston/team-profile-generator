7df7370d122ebadb403215948fef8a8c77cc780a
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-02-25 13:32:23 -0600

    more failed manager tests

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,7 +1,19 @@
 const Manager = require('../lib/Manager');
 
 test('creates an intern object', () => {
-    const manager = new Manager("Ashley", 1, "testing@abc.com", 555-555-5555)
+    const manager = new Manager("Ashley", 1, "testing@abc.com", 555-555-5555);
 
-    expect(manager.number).toEqual(expect.any(Number));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+})
+
+test("gets manger's office number", () => {
+    const manager = new Manager("Ashley", 1, "testing@abc.com", 555-555-5555);
+
+    expect(manager.getOfficeNumber()).toEqual(expect.any(Number));
+})
+
+test("gets manger's role", () => {
+    const manager = new Manager("Ashley", 1, "testing@abc.com", 555-555-5555);
+
+    expect(manager.getRole()).toEqual("Manger");
 })
