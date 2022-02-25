3784ea555cba63f7ba1e2ce7a87dfbc21b71382a
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-02-25 11:58:24 -0600

    passing manager test

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,7 +1,7 @@
 const Manager = require('../lib/Manager');
 
 test('creates an intern object', () => {
-    const manager = new Manager("Ashley", 1, "testing@abc.com", number)
+    const manager = new Manager("Ashley", 1, "testing@abc.com", 555-555-5555)
 
     expect(manager.number).toEqual(expect.any(Number));
 })
